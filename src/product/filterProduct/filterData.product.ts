ddf15363b0f153a67bf4590139c9afd348db95fa
vergaraCoder <jhonatanvergara072@gmail.com>
2024-10-01 07:41:24 -0500

    feat:finish filter of product data

--- src/product/filterProduct/filterData.product.ts
+++ src/product/filterProduct/filterData.product.ts
@@ -5,16 +5,31 @@ import { querysProduct } from "src/common/interface/querysProduct";
 import { operatorsSql } from "src/common/interface/operator";
 
 
+enum sort{
+    asc="ASC",
+    desc="DESC",
+    ASC="ASC",
+    DESC="DESC",
+}
+
 @Injectable()
 export class FilterProductService{
+  
+
     async returnResults(querys:any,repoProduct:Repository<Product>){
         const queryBuilder=repoProduct.createQueryBuilder("products");
         return await this.filterData(querys,queryBuilder);
         
     }
 
+
     private async filterData(querys:querysProduct,builder:SelectQueryBuilder<Product>){   
         let elements=[];     
+        const limit = querys.limit ? querys.limit : null;
+        const skip = querys.page ? querys.page : null;
+        const order= querys.order ? querys.order : null;
+        const sortElection= sort[querys.sort]; // take some value of the enum 
+
         for(let x of Object.keys(querys)){  
             if(querys[x] !== undefined && /^stock_(gt|gte|lt|lte)$/.test(x)){                                                   
                 elements=this.operators(x);                     
@@ -26,12 +41,18 @@ export class FilterProductService{
                 builder.andWhere(`products.price ${elements[1]} ${parseInt(querys[x])}`);
             }
         }
+
         querys.name != undefined ? builder.andWhere("products.name =:name",{name:querys.name}) : "";
-        if(querys.stock){
-            builder.innerJoinAndSelect("products.stock","stock");
-            builder.andWhere
-        }
-        return await builder.getMany();
+      
+        if(limit && skip){
+            builder.skip((skip-1) * limit );
+            builder.take(limit);
+        } 
+        if(limit) builder.take(limit);
+
+        if(sortElection && order) builder.orderBy(`products.${order}`,sortElection);
+ 
+        return await builder.getMany(); // take all register filters
     }
 
     private operators(query:any){
@@ -39,7 +60,6 @@ export class FilterProductService{
         if(coinditentials !== null){
             const operator = operatorsSql[coinditentials[0]];
             const propertir= coinditentials.input.split("_")[0];
-            
             return [operator,propertir];
         }
     }
